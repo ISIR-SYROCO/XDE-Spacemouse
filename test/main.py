ec2e7a785e0a4a487ee3b48bdb9e16b0f9456d30
shak <sovannara.hak@isir.upmc.fr>
2013-02-05 11:16:49 +0100

    Update test/main.py

--- test/main.py
+++ test/main.py
@@ -21,9 +21,11 @@ print "BEGIN OF SCRIPT..."
 
 TIME_STEP = .01
 
+import xde_resources as xr
 
 import xde_robot_loader as xrl
-clock, phy, graph = xwm.createAllAgents(TIME_STEP)
+wm = xwm.WorldManager()
+wm.createAllAgents(TIME_STEP)
 
 
 import xde_spacemouse as spacemouse
@@ -31,42 +33,40 @@ import xde_spacemouse as spacemouse
 
 #######################################################################################################
 print "START ALL..."
-import lgsm
 
+sphereWorld = xrl.createWorldFromUrdfFile(xr.sphere, "sphere", [0,0.6,1.2, 1, 0, 0, 0], False, 0.2, 0.005)# , "material.concrete")
+kukaWorld = xrl.createWorldFromUrdfFile(xr.kuka, "k1g", [0,0,0.4, 1, 0, 0, 0], True, 1, 0.005) #, "material.concrete")
 
-sphereWorld = xrl.createWorldFromUrdfFile("resources/urdf/sphere.xml", "sphere", [0,0.6,1.2, 1, 0, 0, 0], False, 0.2, 0.005)# , "material.concrete")
-kukaWorld = xrl.createWorldFromUrdfFile("resources/urdf/kuka.xml", "k1g", [0,0,0.4, 1, 0, 0, 0], True, 1, 0.005) #, "material.concrete")
-
-xwm.addMarkers(sphereWorld, ["spheresphere"], thin_markers=False)
-xwm.addWorld(sphereWorld, True)
-xwm.addWorld(kukaWorld, True)
+wm.addMarkers(sphereWorld, ["sphere.sphere"], thin_markers=False)
+wm.addWorld(sphereWorld, True)
+wm.addWorld(kukaWorld, True)
 
 
 # Create simple gravity compensator controller
 import control
 controller = control.createTask("controlK1G", TIME_STEP)
-controller.connectToRobot(phy, kukaWorld, "k1g")
+controller.connectToRobot(wm.phy, kukaWorld, "k1g")
 
 # Configure the robot
-import numpy as np
-kuka = phy.s.GVM.Robot("k1g")
+import lgsm
+kuka = wm.phy.s.GVM.Robot("k1g")
 kuka.enableGravity(True)
-kuka.setJointPositions(np.array([.4]*7).reshape(7,1))
-kuka.setJointVelocities(np.array([0.0]*7).reshape(7,1))
-#kuka.enableContactWithBody("spheresphere", True)
+kuka.setJointPositions(lgsm.vector([0.4]*7))
+kuka.setJointVelocities(lgsm.vector([0.0]*7))
+#kuka.enableContactWithBody("sphere.sphere", True)
 
 
 controller.s.start()
 
 #PDC Control mode
-#sm = spacemouse.createTask("smi", TIME_STEP, phy, graph, "spheresphere", pdc_enabled=True, body_name="07k1g")
+sm = spacemouse.createTask("smi", TIME_STEP, wm.phy, wm.graph, "sphere.sphere", pdc_enabled=True, body_name="k1g.07")
 
 # Normal mode
-sm = spacemouse.createTask("smi", TIME_STEP, phy, graph, "spheresphere", pdc_enabled=False)
+#sm = spacemouse.createTask("smi", TIME_STEP, wm.phy, wm.graph, "sphere.sphere", pdc_enabled=False)
 
 sm.s.start()
 
-phy.s.startSimulation()
+wm.startSimulation()
 
 shell()
 
